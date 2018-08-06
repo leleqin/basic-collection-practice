82e36d05bb1c701d927ac7a6ea827cca44a81aa8
leleqin <602568224@qq.com>
2018-08-06 17:39:57 +0800

    finish

--- main/section-3/practice-3.js
+++ main/section-3/practice-3.js
@@ -1,5 +1,43 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    let sameElementCount = countSameElements(collectionA);
+    updatedCollection(sameElementCount, objectB);
+
+    return sameElementCount;
+}
+
+function updatedCollection(collectionA, objectB) {
+    let objectBValues = objectB.value;
+
+    collectionA.map(element => {
+        if (objectBValues.includes(element.key))
+            if (element.count >= 3) element.count -= parseInt(element.count / 3);
+    });
+
+    return collectionA;
+}
+
+function countSameElements(collection) {
+    let sameElementCount = [];
+
+    collection.map(key => {
+        let keyIndex = findContainsKeyIndex(sameElementCount, key);
+        if (keyIndex !== -1) addKeyCount(sameElementCount, keyIndex);
+        else sameElementCount.push(createObj(key));
+    });
+
+    return sameElementCount;
+}
+
+function findContainsKeyIndex(sameElementCount, key) {
+    return sameElementCount.findIndex(element => element.key === key);
+}
+
+function addKeyCount(sameElementCount, keyIndex) {
+    sameElementCount[keyIndex].count += 1;
+}
+
+function createObj(key) {
+    return { key: key, count: 1 };
 }
--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,5 +1,51 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    var result = [];
+    var index = 0;
+    var sum = 0;
+    while (true) {
+        var count = 0;
+        index = sum;
+        for (var i in collectionA) {
+            if (collectionA[index] === collectionA[i]) {
+                count++;
+            }
+        }
+        sum += count;
+        if (collectionA[index].split("-").length === 1) {
+            result.push({ key: collectionA[index], count: count });
+        } else {
+            result.push({
+                key: collectionA[index].split("-")[0],
+                count: collectionA[index].split("-")[1]
+            });
+        }
+
+        if (sum >= collectionA.length) {
+            break;
+        }
+    }
+
+    return sub(result, objectB);
+}
+
+function sub(collection, object) {
+    for (var i in collection) {
+        for (var j in object.value) {
+            if (collection[i].key === object.value[j]) {
+                if (collection[i].count % 3 === 0) {
+                    collection[i].count = collection[i].count -= collection[i].count / 3;
+                } else if (collection[i].count % 3 === 1) {
+                    collection[i].count = collection[i].count -=
+                        (collection[i].count - 1) / 3;
+                } else if (collection[i].count % 3 === 2) {
+                    collection[i].count = collection[i].count -=
+                        (collection[i].count - 2) / 3;
+                }
+            }
+        }
+    }
+
+    return collection;
 }
