3de4a2f91726dc719ee02cebbbc33fc704ae2e31
leleqin <602568224@qq.com>
2018-08-06 16:46:10 +0800

    finish section-1

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = collectionA.map(item => item.key).filter(data => objectB.value.includes(data));
+  return result;
 }
